30406d985c417b51a297d40cf7c0be8d514b85c5
AnaJL <analinsdev@gmail.com>
2021-03-03 15:48:50 -0300

    retirando html e adicionando informações com componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ana Julia Lins';
+  nome = 'Ana Julia Lins';
+  disciplina = 'PWeb I';
 }
